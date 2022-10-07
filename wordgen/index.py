c4ded247578fce8830be5b0c2075b16ba074d426
Alfio Ferrara <alfio.ferrara@unimi.it>
2022-10-07 17:12:41 +0200

    Update lecture 3 2022

--- wordgen/index.py
+++ wordgen/index.py
@@ -15,3 +15,42 @@ class WordIndex(object):
                 self.index[word] += 1
 
 
+class UnigramIndex(object):
+    """
+    Provides unigram probability estimation
+    (eventually) given a word index
+    """
+    def __init__(self):
+        self.index = defaultdict(lambda: 0)
+
+    def index_word(self, word: str, frequency: int = 1,
+                   lower_case: bool = False):
+        if lower_case:
+            w = word.lower()
+        else:
+            w = word
+        for c in w:
+            self.index[c] += frequency
+        self.index[' '] += frequency
+
+    def __getitem__(self, char: str):
+        return self.index[char]
+
+    @staticmethod
+    def from_word_index(word_index: WordIndex,
+                        lower_case: bool = False):
+        """
+        A static method that provides and initialization mechanism
+        for UnigramIndex
+        :param word_index: the word index
+        :param lower_case: check if lowercase (default false)
+        :return: an instance of UnigramIndex
+        """
+        i = UnigramIndex()
+        for word, frequency in word_index.index.items():
+            i.index_word(word, frequency=frequency, lower_case=lower_case)
+        return i
+
+
+
+
